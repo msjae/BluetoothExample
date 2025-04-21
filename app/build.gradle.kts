3e584468c384b927c30c04276bb691bb6f0512f7
SeongjaeMin <mugun19@kookmin.ac.kr>
2025-04-21 17:58:36 +0900

    wearOS 4.0 (Android 13 (API 레벨 33)) 에 맞게 수정 및 블루투스 권한 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.cookandroid.bluetoothexample"
-        minSdk = 30
+        minSdk = 33
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
